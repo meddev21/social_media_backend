6af933a948ae354ad3494ead98ca2995cb65937a
meddev21 <touati.tliba.mohammed@gmail.com>
2023-10-09 10:42:55 +0100

    deploying to railway 10:42 09/10/2023

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(p0_euu3(epvmz25kc5#^+7r(-65j53i@#d-gk2gk%o!h8#grj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -126,6 +126,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 GRAPHENE = {
     'SCHEMA': 'core.schema.schema', # this file doesn't exist yet
--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+
+    email = models.EmailField(blank=False, max_length=254, verbose_name="email address")
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    USERNAME_FIELD = "username"
+    EMAIL_FIELD = "email"
